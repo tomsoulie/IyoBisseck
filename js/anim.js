879f9a676b6e7155b0a749f8a5fda5b2313092be
tomsoulie <56165268+tomsoulie@users.noreply.github.com>
2022-10-25 11:10:47 +0200

    Add files via upload

--- js/anim.js
+++ js/anim.js
@@ -5,9 +5,10 @@ let btReduire = document.querySelector("#reduire");
 let displayGrid = document.querySelector(".displayGrid");
 let grid = document.querySelector(".columnTest");
 let toogle = 0;
+let toogleVid = 0;
 
 let bt = document.querySelector(".bt"); // Bouton bleu
-
+let vid = document.querySelector(".vid");
 
 
 // afficher et masquer la grille lorqu'on clique sur GRILLE
@@ -26,11 +27,14 @@ displayGrid.onclick = function(){
 // Afficher une video ?? quand on clique sur le bouton bleu
 bt.onclick = function(){
 
-  if (toogle == 0) {
+  if (toogleVid == 0) {
     // Afficher la video?? ou autre chose
-    
+    vid.style.display = "block";
+    toogleVid = toogleVid+1;
   } else {
     // Masquer cette dernière
+    vid.style.display = "none";
+    toogleVid = toogleVid-1;
   }
 }	
 
